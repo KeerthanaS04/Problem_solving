61a6ee02ec0f122eaddad8f2aa9f8d855ab97b2b
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2023-06-09 21:17:33 +0530

    Add files via upload

---
+++ Codestudio/wavePrint.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+vector<int> wavePrint(vector<vector<int>> arr,int nRows,int mCols){
+    vector<int> ans;
+    for(int col=0;col<mCols;col++){
+        if(col&1){
+            //odd index->Bottom to top
+            for(int row=nRows-1;row>=0;row--){
+                ans.push_back(arr[row][col]);
+            }
+        }else{
+            for(int row=0;row<nRows;row++){
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}
