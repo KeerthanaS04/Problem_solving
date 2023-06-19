399364490ee27365b77b37090615cda8a87e1b05
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2023-06-19 22:24:42 +0530

    Create ratMaze.cpp

---
+++ GFG/ratMaze.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+class Solution{
+    private:
+    bool isSafe(int x,int y,int n,vector<vector<int>> visited,vector<vector<int>> &m){
+        if((x>=0 && x<n)&&(y>=0 && y<n)&&(visited[x][y]==0 && m[x][y]==1)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    void solve(vector<vector<int>> &m,int n,vector<string>& ans,int x,int y,vector<vector<int>> visited,string path){
+        //you have reached x,y here
+        //base case
+        if(x==n-1 && y==n-1){
+            ans.push_back(path);
+            return ;
+        }
+        visited[x][y]=1;
+        //4 choices - D,L,R,U
+        
+        //down
+        int newx=x+1;
+        int newy=y;
+        if(isSafe(newx,newy,n,visited,m)){
+            path.push_back('D');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back(); // to remove the mislead
+        }
+        
+        //left
+        newx=x;
+        newy=y-1;
+        if(isSafe(newx,newy,n,visited,m)){
+            path.push_back('L');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back(); // to remove the mislead
+        }
+        
+        //right
+        newx=x;
+        newy=y+1;
+        if(isSafe(newx,newy,n,visited,m)){
+            path.push_back('R');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back(); // to remove the mislead
+        }
+        
+        //up
+        newx=x-1;
+        newy=y;
+        if(isSafe(newx,newy,n,visited,m)){
+            path.push_back('U');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back(); // to remove the mislead
+        }
+        visited[x][y]=0;
+    }
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        int srcx=0;
+        int srcy =0;
+        
+        if(m[0][0]==0){
+            return ans;
+        }
+        
+        vector<vector<int>> visited=m;
+        //Initialise with 0
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                visited[i][j]=0;
+            }
+        }
+        
+        string path="";
+        solve(m,n,ans,srcx,srcy,visited,path);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
