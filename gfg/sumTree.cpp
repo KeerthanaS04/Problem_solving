e37fcba876e009127f1607035101ae9d9c14b64e
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2023-08-01 12:29:23 +0530

    Add files via upload

---
+++ gfg/sumTree.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left,*right;
+};
+
+class Solution
+{
+    public:
+    pair<bool,int> isSumTreeFast(Node* root){
+        //base case
+        if(root==NULL){
+            pair<bool,int> p=make_pair(true,0);
+            return p;
+        }
+        if(root->left==NULL && root->right==NULL){
+            pair<bool,int> p=make_pair(true,root->data);
+            return p;
+        }
+        
+        pair<bool,int> leftAns=isSumTreeFast(root->left);
+        pair<bool,int> rightAns=isSumTreeFast(root->right);
+        
+        bool isLeftSumTree=leftAns.first;
+        bool isRightSumTree=rightAns.first;
+        
+        int leftSum=leftAns.second;
+        int rightSum=rightAns.second;
+        
+        bool condn=root->data==leftSum+rightSum;
+        
+        pair<bool,int> ans;
+        
+        if(isLeftSumTree && isRightSumTree && condn){
+            ans.first=true;
+            ans.second=root->data+leftSum+rightSum;
+        }else{
+            ans.first=false;
+        }
+        return ans;
+    }
+    bool isSumTree(Node* root)
+    {
+         // Your code here
+         return isSumTreeFast(root).first;
+    }
+};
