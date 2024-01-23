c449bc4da6a16a6d492aa3eeeee9ff71cc777274
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2024-01-23 19:15:08 +0530

    Add files via upload

---
+++ DailyProblem/GFG/PathsFromRootWithAspecifiedSum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespacestd;
+
+class Solution{
+public:
+    void help(Node* root,int s,int sum,vector<int> temp,vector<vector<int>>& ans){
+        if(!root) return;
+        s+=root->key;
+        temp.push_back(root->key);
+        if(s==sum) ans.push_back(temp);
+        help(root->left,s,sum,temp,ans);
+        help(root->right,s,sum,temp,ans);
+    }
+    vector<vector<int>> printPaths(Node* root,int sum){
+        vector<vector<int>> ans;
+        vector<int> temp;
+        help(root,0,sum,temp,ans);
+        return ans;
+    }
+};
