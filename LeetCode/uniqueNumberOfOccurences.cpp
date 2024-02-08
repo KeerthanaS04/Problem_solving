ef1319188f34290e6bc6cc3b5ad70c8b679750d3
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2024-02-08 10:18:03 +0530

    Add files via upload

---
+++ LeetCode/uniqueNumberOfOccurences.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    bool uniqueOccurence(vector<int>& arr){
+        vector<int> ans;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            int count=1;
+            for(j=i+1;j<arr.size();j++){
+                if(arr[i]==arr[j+1]){
+                    count++;
+                }
+            }
+            ans.push_back(count);
+            i+=count;
+        }
+
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<ans.size()-1;i++){
+            if(arr[i]==arr[i+1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
