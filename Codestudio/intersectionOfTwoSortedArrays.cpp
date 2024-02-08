a3014a130c3298c54fe56dbe2e36cd4d3dd4d46f
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2024-02-08 13:17:36 +0530

    Add files via upload

---
+++ Codestudio/intersectionOfTwoSortedArrays.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findArrayIntersection(vector<int>& arr1,int n,vector<int>& arr2,int m){
+    vector<int> ans;
+    int i=0, j=0;
+    while(i<n && j<m){
+        if(arr1[i]==arr2[j]){
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }else if(arr1[i]<arr2[j]){
+            i++;
+        }else{
+            j++;
+        }
+    }
+    return ans;
+}
