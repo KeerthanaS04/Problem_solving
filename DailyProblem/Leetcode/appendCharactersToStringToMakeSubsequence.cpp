32a744cb57aea3ac17663d0b9344f287248e6bfe
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2024-06-04 00:15:50 +0530

    Add files via upload

---
+++ DailyProblem/Leetcode/appendCharactersToStringToMakeSubsequence.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int appendCharacters(string s,string t){
+        int m=s.size();
+        int n=t.size();
+        for(int i=0,j=0;j<n;j++){
+            while(i<m && s[i]!=t[j]){
+                i++;
+            }
+            if(i==m){
+                return n-j;
+            }
+            i++;
+        }
+        return 0;
+    }
+};
