ca52b5a1280cec4223608732a54e1b0c0d844d16
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2024-06-04 00:15:18 +0530

    Add files via upload

---
+++ DailyProblem/GFG/TrailOfOnes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int numberOfConsecutiveOnes(int n){
+        if(n==2) return 1;
+        int a=0,b=1,ans=1,mod=1e9+7;
+        for(int i=3;i<=n;i++){
+            int c=(a+b)%mod;
+            a=b;
+            b=c;
+            ans=((ans*2)%mod+c)%mod;
+        }
+        return ans;
+    }
+};
