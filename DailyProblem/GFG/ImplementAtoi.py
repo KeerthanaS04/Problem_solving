fb9817ff7afb5434de9ec0c1c1e01ed8cd176ee6
KEERTHANA S <keerthanasmallika@gmail.com>
2026-04-16 23:52:33 +0530

    POTD - 16/04/2026

---
+++ DailyProblem/GFG/ImplementAtoi.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myAtoi(self, s):
+        idx, sign, res = 0, 1, 0
+
+        while idx<len(s) and s[idx]==' ':
+            idx+=1
+        
+        if idx<len(s) and (s[idx]=='-' or s[idx]=='+'):
+            sign=-1 if s[idx]=='-' else 1
+            idx+=1
+        
+        while idx<len(s) and '0'<=s[idx]<='9':
+            res = res*10+(ord(s[idx])-ord('0'))
+            idx+=1
+
+            if sign*res>2**31-1:
+                return 2**31-1
+            if sign*res<-2**31:
+                return -2**31
+        return sign*res
