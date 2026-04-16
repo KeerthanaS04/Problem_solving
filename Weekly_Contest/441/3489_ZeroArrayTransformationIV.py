3a2dd02501345073afd9933617ce908b05393e5f
KEERTHANA S <keerthanasmallika@gmail.com>
2026-04-17 00:31:28 +0530

    Weekly Contest - 441

---
+++ Weekly_Contest/441/3489_ZeroArrayTransformationIV.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        if all(num==0 for num in nums):
+            return 0
+        
+        n = len(nums)
+        subsetSums = [{} for _ in range(n)]
+
+        for k, (l, r, val) in enumerate(queries):
+            for i in range(l, r+1):
+                newSums = {subsetSum+val for subsetSum in subsetSums[i]}
+                subsetSums[i].update(newSums)
+            if all(nums[i] in subsetSums[i] for i in range(n)):
+                return k+1
+        return -1
