69c4d6f98559ca9815588365efd422efb5cce2cc
KEERTHANA S <keerthanasmallika@gmail.com>
2026-04-21 02:04:40 +0530

    Additional - LC Beginner's Guide

---
+++ LeetCode/2139_MinimumMovesToReachTargetScore.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        if target==1:
+            return 0
+        if maxDoubles==0:
+            return target-1
+        
+        if target%2==0 and maxDoubles>0:
+            return 1+self.minMoves(target//2, maxDoubles-1)
+        return 1+self.minMoves(target-1, maxDoubles)
