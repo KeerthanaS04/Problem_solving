90906d161bdf9c58f5070b59a8d8a058558a6a33
KEERTHANA S <keerthanasmallika@gmail.com>
2026-05-24 00:29:13 +0530

    Additional

---
+++ LeetCode/33_SearchInRotatedSortedArray.py
@@ -0,0 +1,25 @@
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+
+        while l<=r:
+            mid = (l+r)//2
+
+            if nums[mid]==target:
+                return mid
+
+            # check which half is sorted
+            if nums[l]<=nums[mid]:
+                # left half is sorted
+                if nums[l]<=target<nums[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
+            else:
+                # right half is sorted
+                if nums[mid]<target<=nums[r]:
+                    l = mid+1
+                else:
+                    r = mid-1
+        return -1
