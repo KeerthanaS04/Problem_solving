4a5e4c09f91e6bdd96858048dc0d7b2b45ab151a
KEERTHANA S <keerthanasmallika@gmail.com>
2026-07-20 00:54:58 +0530

    POTD - 19/07/2026

---
+++ DailyProblem/Leetcode/1081_SmallestSubsequenceOfDistinctCharacters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        last_occurence = {char:i for i, char in enumerate(s)}
+        stack = []
+        visited = set()
+
+        for i, char in enumerate(s):
+            if char in visited:
+                continue
+
+            while stack and stack[-1]>char and last_occurence[stack[-1]]>i:
+                removed_char = stack.pop()
+                visited.remove(removed_char)
+            stack.append(char)
+            visited.add(char)
+        return ''.join(stack)
