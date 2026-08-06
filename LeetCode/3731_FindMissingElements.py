ba9a1877881c368243e50d3241359cfc9c31e742
KEERTHANA S <keerthanasmallika@gmail.com>
2026-08-06 23:59:51 +0530

    Additional

---
+++ LeetCode/3731_FindMissingElements.py
@@ -0,0 +1,6 @@
+from typing import List
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        mn, mx = min(nums), max(nums)
+        s = set(nums)
+        return [x for x in range(mn+1, mx) if x not in s]
